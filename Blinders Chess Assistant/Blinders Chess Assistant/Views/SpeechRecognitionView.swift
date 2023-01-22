efc0e4ce49ae374dc582ce2573dd815e661c67d6
S3ntin3l <yleclech@ordinateur-portable-de-yohann.home>
2023-01-22 20:02:14 +0100

    Vocal assistant integration

---
+++ Blinders Chess Assistant/Blinders Chess Assistant/Views/SpeechRecognitionView.swift
@@ -0,0 +1,20 @@
+//
+//  SpeechRecognitionView.swift
+//  
+//
+//  Created by Yohann Le Clech on 22/01/2023.
+//
+
+import SwiftUI
+
+struct SpeechRecognitionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpeechRecognitionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeechRecognitionView()
+    }
+}
